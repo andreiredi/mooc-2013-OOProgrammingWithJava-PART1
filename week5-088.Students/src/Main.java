68200ee16002aaf617cffc6707e7c5834cc1fe67
Andrei <andrei.redi@khk.ee>
2018-10-31 14:58:10 +0200

    Ülesanne 88

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -5,6 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write here the main program
+     Scanner reader = new Scanner(System.in);
+     ArrayList<Student> list = new ArrayList<Student>();
+     String name;
+     String studentnumber;
+
+     while (true) {
+         System.out.println("name: ");
+         name = reader.nextLine();
+
+         if(name.isEmpty())
+             break;
+
+         System.out.println("studentnumber: ");
+         studentnumber = reader.nextLine();
+
+         Student buffer = new Student(name, studentnumber);
+
+         for (Student i : list) {
+             System.out.println(i.toString());
+         }
+     }
     }
 }
